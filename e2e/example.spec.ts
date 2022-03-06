69214a3e56447f0038abf942713db373d500f732
Franziska Hinkelmann <franzih@chromium.org>
2022-03-06 11:39:57 -0500

    Add baseURL config option

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -1,8 +1,8 @@
 import { test, expect } from '@playwright/test'
 
-test('should navigate to the react page', async ({ page }) => {
+test('should navigate to the react page', async ({ page, baseURL }) => {
     // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
-    await page.goto('http://localhost:3000/')
+    await page.goto(baseURL)
     // Find an element with the text 'About Page' and click on it
     await page.click('text=React')
     // The new url should be "/project/facebook-react/" (baseURL is used there)
