984fe488a3989201e28be6687227223076a0cdd9
Franziska Hinkelmann <franzih@chromium.org>
2022-03-06 11:40:45 -0500

    Use relative links in tests

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -1,8 +1,8 @@
 import { test, expect } from '@playwright/test'
 
-test('should navigate to the react page', async ({ page, baseURL }) => {
+test('should navigate to the react page', async ({ page }) => {
     // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
-    await page.goto(baseURL)
+    await page.goto('/')
     // Find an element with the text 'About Page' and click on it
     await page.click('text=React')
     // The new url should be "/project/facebook-react/" (baseURL is used there)
