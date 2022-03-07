c41105581c3c84ae8ccdd74c95bc6c484c6fa639
Franziska Hinkelmann <franzih@chromium.org>
2022-03-06 21:40:13 -0500

    Random greetings in function

--- api/message/index.js
+++ api/message/index.js
@@ -1,7 +1,8 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
+    const greetings = ["hello", "hola", "hallo", "ciao"];
     context.res.json({
-        text: "Hello from the API"
+        text: greetings[Math.floor(Math.random()*greetings.length)],
     });
 }
